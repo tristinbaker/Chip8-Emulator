b45bc5b9d8edffad350536734237cd03220943a4
Tristin Baker <tristinbaker52@gmail.com>
2026-09-18 07:11:44 -0400

    readme updates, opcode implementation begins

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Chip 8 Emulator
+## Chip 8 Emulator
 
 This is a repo of my work on a Chip 8 emulator written in Rust. The goals of this project are as follows:
   * Reaquaint myself with lower level programming after 8 years of web development using Ruby/JS/Python/etc.
@@ -7,3 +7,13 @@ This is a repo of my work on a Chip 8 emulator written in Rust. The goals of thi
   * Finally, write code with no AI. I haven't written much code without the assistance of AI for at least a year, and I'm afraid of the long term effects of this, so I want to write code personally, so that I keep my skills sharp enough and actually understand what I am writing.
 
 I am following [this guide](https://github.com/aquova/chip8-book) for this, so this is not entirely handwritten code, or maybe even not at all. But that's okay with me, because I am trying to fully understand what's going on in emulation at a deeper level, and this is the natural first step.
+
+#### Notes on comments in code: 
+
+I am overly commenting the code for my own sake. It helps me when re-reading this code (which I will be doing as I work on the GB emulator) to remember _exactly_ what each line is doing. I believe this will be very useful in the long run and will greatly assist in writing opcodes specifically for the GB.
+
+#### Albums listened to while programming: 
+
+* [Into the Wild (Music For The Motion Picture) by Eddie Vedder](https://music.youtube.com/playlist?list=OLAK5uy_k-NKMjAPoDEbQt4z6AAkMtA0Th3aGqAuY)
+* [Morning Phase by Beck](https://music.youtube.com/playlist?list=OLAK5uy_kmDsbDzEsmwtbxsla6Gw3_1P5yV2t8zOU)
+* [My Days of 58 by Bill Callahan](https://music.youtube.com/playlist?list=OLAK5uy_mz83fBMJH5Fjw7R646W9v1eyQrri3rcbM)
--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -88,6 +88,200 @@ impl Emu {
         self.st = 0;
         self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
     }
+
+    pub fn tick(&mut self) {
+        // Fetch the opcode
+        let op = self.fetch();
+
+        // Decode the opcode
+        // Execute the opcode
+        // Done in one function
+        self.execute(op);
+    }
+
+    // Fetches the opcode from RAM so it can be decoded and executed
+    // All opcodes in Chip-8 are 2 bytes by definition
+    // For example: 
+    // Address: 0x200 0x201 0x202 0x203 0x204 0x205
+    // Bytes:   0xA2  0x2A  0x60  0x0C  0xD0  0x11
+    // Higher Byte: 0x00A2
+    // Lower Byte:  0x002A
+    // Higher Byte << 8: 0xA200
+    // 0xA200 | 0x002A: 0xA22A -> Full Opcode
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+
+    // Decrements both timers down once every frame, instead of once every CPU cycle
+    // If st > 0, the emulator emits a beep
+    // TODO: Beep when st > 0
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
+        }
+
+        if self.st > 0 {
+            // Beep
+        }
+         self.st -= 1;
+    }
+
+    // Decodes and Executes the opcode, the main meat of the emulator
+    // Using our example from above, op = 0xA22A
+    fn execute(&mut self, op: u16) {
+        let digit1 = (op & 0xF000) >> 12; // (0xA22A & 0xF000) -> 0xA000, 0xA000 >> 12 -> 0xA
+        let digit2 = (op & 0x0F00) >> 8;  // (0xA22A & 0x0F00) -> 0x0200, 0x0200 >> 8 ->  0x2
+        let digit3 = (op & 0x00F0) >> 4;  // (0xA22A & 0x00F0) -> 0x0020, 0x0020 >> 4 ->  0x2
+        let digit4 = op & 0x000F;         // (0xA22A & 0x000F) -> 0x000A, 0x000A >> 0 ->  0xA
+
+        match (digit1, digit2, digit3, digit4) {
+            // 0000:  NOP
+            (0, 0, 0, 0) => return,
+
+            // 00E0:  CLS
+            (0, 0, 0xE, 0) => {
+            }
+
+            // 00EE:  RET
+            (0, 0, 0xE, 0xE) => {
+            }
+
+            // 1NNN:  JMP NNN
+            (1, _, _, _) => {
+            }
+
+            // 2NNN:  CALL NNN
+            (2, _, _, _) => {
+            }
+
+            // 3XNN:  SKIP VX == NN
+            (3, _, _, _) => {
+            }
+
+            // 4XNN:  SKIP VX != NN
+            (4, _, _, _) => {
+            }
+
+            // 5XY0:  SKIP VX == VY
+            (5, _, _, 0) => {
+            }
+
+            // 6XNN:  VX = NN
+            (6, _, _, _) => {
+            }
+
+            // 7XNN:  VX += NN
+            (7, _, _, _) => {
+            }
+
+            // 8XY0:  VX = VY
+            (8, _, _, 0) => {
+            }
+
+            // 8XY1:  VX |= VY
+            (8, _, _, 1) => {
+            }
+
+            // 8XY2:  VX &= VY
+            (8, _, _, 2) => {
+            }
+
+            // 8XY3:  VX ^= VY
+            (8, _, _, 3) => {
+            }
+
+            // 8XY4:  VX += VY
+            (8, _, _, 4) => {
+            }
+
+            // 8XY5:  VX -= VY
+            (8, _, _, 5) => {
+            }
+
+            // 8XY6:  VX >>= 1
+            (8, _, _, 6) => {
+            }
+
+            // 8XY7:  VX = VY - VX
+            (8, _, _, 7) => {
+            }
+
+            // 8XYE:  VX <<= 1
+            (8, _, _, 0xE) => {
+            }
+
+            // 9XY0:  SKIP VX != VY
+            (9, _, _, 0) => {
+            }
+
+            // ANNN:  I = NNN
+            (0xA, _, _, _) => {
+            }
+            
+            // BNNN:  JMP V0 + NNN
+            (0xB, _, _, _) => {
+            }
+            
+            // CXNN:  VX = rand() & NN
+            (0xC, _, _, _) => {
+            }
+            
+            // DXYN:  DRAW
+            (0xD, _, _, _) => {
+            }
+            
+            // EX9E:  SKIP KEY PRESS
+            (0xE, _, 9, 0xE) => {
+            }
+            
+            // EXA1:  SKIP KEY RELEASE
+            (0xE, _, 0xA, 1) => {
+            }
+            
+            // FX07:  VX = DT
+            (0xF, _, 0, 7) => {
+            }
+            
+            // FX0A:  WAIT KEY
+            (0xF, _, 0, 0xA) => {
+            }
+            
+            // FX15:  DT = VX
+            (0xF, _, 1, 5) => {
+            }
+            
+            // FX18:  ST = VX
+            (0xF, _, 1, 8) => {
+            }
+            
+            // FX1E:  I += VX
+            (0xF, _, 1, 0xE) => {
+            }
+            
+            // FX29:  I = FONT
+            (0xF, _, 2, 9) => {
+            }
+            
+            // FX33:  BCD
+            (0xF, _, 3, 3) => {
+            }
+            
+            // FX55:  STO V0 - VX
+            (0xF, _, 5, 5) => {
+            }
+            
+            // FX65:  LD V0 - VX
+            (0xF, _, 6, 5) => {
+            }
+            
+            // the fallback state in case we reach an opcode that is unimplemented
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
+        }
+    }
 }
 
 
